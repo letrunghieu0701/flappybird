c031414bd6e3e1e141c65318145eb0e8bed4abdc
Hieu Le Trung <hieu.le@gihot.vn>
2022-03-21 17:22:26 +0700

    Refactoring code for ScoreLine

--- Assets/Scripts/ScoreLine.cs
+++ Assets/Scripts/ScoreLine.cs
@@ -7,29 +7,19 @@ using XLua;
 
 public class ScoreLine : MonoBehaviour
 {
-    LuaEnv luaEnv = null;
-    LuaTable scriptEnv = null;
     public TextAsset luaScript = null;
     OnTriggerEnter2DDelegate luaOnTriggerEnter2D = null;
     StartDelegate luaStart = null;
 
     void Awake()
     {
-        luaEnv = new LuaEnv();
-        scriptEnv = luaEnv.NewTable();
-
-        LuaTable meta = luaEnv.NewTable();
-        meta.Set("__index", luaEnv.Global);
-        scriptEnv.SetMetaTable(meta);
-        meta.Dispose();
-
+        LuaTable scriptEnv = XLuaEnvironment.Instance.CreateScriptEnv();
         scriptEnv.Set("self", this);
 
-        luaEnv.DoString(luaScript.text, luaScript.name, scriptEnv);
+        XLuaEnvironment.luaEnv.DoString(luaScript.text, luaScript.name, scriptEnv);
 
         luaStart = scriptEnv.Get<StartDelegate>("LuaStart");
         luaOnTriggerEnter2D = scriptEnv.Get<OnTriggerEnter2DDelegate>("LuaOnTriggerEnter2D");
-
     }
 
     void Start()
